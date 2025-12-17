03fff5018cef602c338bebb89fd0f96556550b61
adrianogetme <adrian.belah@gmail.com>
2025-12-17 03:23:39 +0200

    Fix file names and main.py imports

--- ab24143-main.py
+++ ab24143-main.py
@@ -1,7 +1,7 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from ab24143-libadd import *
-from ab24143-libmult import *
+from libadd import *
+from libmult import *
 a = 13
 b = 5
 print(plus(a,b))
